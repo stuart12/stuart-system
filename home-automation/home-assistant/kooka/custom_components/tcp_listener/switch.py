4782ddc1f307c636022d18cfec137b93bf8b8628
Stuart Pook <stuart12@users.noreply.github.com>
2019-05-13 01:20:16 +0200

    tcp_listener: replace fork & listen with asyncio-stream

--- home-automation/home-assistant/kooka/custom_components/tcp_listener/switch.py
+++ home-automation/home-assistant/kooka/custom_components/tcp_listener/switch.py
@@ -14,8 +14,8 @@
 #         port: 20569
 #         name: Quiet Telephone
 #
-# TODO: fix "Failed to create HTTP server at port 8123" error after a restart in the UI.
-# (probably caused by none of the *on_remove* methods being called when the restart is triggered).
+# TODO: fix bugs in async & asyncio usage (this is my first async code).
+# TODO: understand async_will_remove_from_hass and async_on_remove
 #
 # Copyright (c) 2019 Stuart Pook (http://www.pook.it/)
 # This program is free software: you can redistribute it and/or modify it under
@@ -30,12 +30,9 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import logging
-import signal
-import sys
-import os
-import socket
+import asyncio
 
-VERSION = '0.0.1'
+VERSION = '0.0.2'
 _LOGGER = logging.getLogger(__name__)
 DOMAIN = 'tcp_listener'
 
@@ -43,10 +40,10 @@ import voluptuous as vol
 
 from homeassistant.components.switch import SwitchDevice, PLATFORM_SCHEMA
 # https://github.com/home-assistant/home-assistant/blob/dev/homeassistant/const.py
-from homeassistant.const import CONF_PORT, CONF_HOST, CONF_NAME, CONF_STATE, STATE_ON, STATE_OFF, CONF_SWITCHES
+from homeassistant.const import CONF_PORT, CONF_NAME, CONF_STATE, STATE_ON, STATE_OFF, CONF_SWITCHES
 import homeassistant.helpers.config_validation as cv
 
-REQUIREMENTS = []
+REQUIREMENTS = [] # needed?
 
 # https://gist.github.com/ispiropoulos/90a5f215e71f4dde635e3e3407fb5804
 SWITCH_SCHEMA = vol.Schema({
@@ -60,10 +57,8 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
 })
 
 async def async_setup_platform(hass, config, async_add_devices, discovery_info=None):
-    _LOGGER.debug("async_setup_platform")
     cfg = config[CONF_SWITCHES]
     _LOGGER.debug("starting with config %s", cfg)
-
     async_add_devices([ TCPListener(hass, p.get(CONF_NAME, name), p[CONF_PORT], p[CONF_STATE]) for name, p in cfg.items() ])
 
 class TCPListener(SwitchDevice):
@@ -72,15 +67,9 @@ class TCPListener(SwitchDevice):
         self._name = name
         self._state = state
         self._port = port
-        self._pid = 0
+        self._server = None
         _LOGGER.debug("new TCPListener with config %s", self)
-        _LOGGER.debug("new TCPListener with methods %s", dir(self))
-        self.async_on_remove(self.on_remove)
-
-    def _switch(self, newstate):
-        """Switch on or off."""
-        _LOGGER.info("Switching %s to state: %s", self.name, newstate)
-        return newstate == 'on'
+        self.async_on_remove(self.on_remove) # is this ever called?
 
     @property
     def name(self):
@@ -95,55 +84,47 @@ class TCPListener(SwitchDevice):
     # https://github.com/home-assistant/home-assistant/blob/dev/homeassistant/components/switch/light.py
 
     async def async_turn_on(self, **kwargs):
-        _LOGGER.debug("async turn on %s pid %d %s", self.name, self._pid, kwargs)
-        if not self._pid:
-            self._pid = self._listen()
+        _LOGGER.debug("async_turn_on %s:%d", self.name, self._port)
         self._state = STATE_ON
+        await self.async_listen()
 
     async def async_turn_off(self, **kwargs):
-        _LOGGER.debug("async turn off %s pid %d %s", self.name, self._pid, kwargs)
+        _LOGGER.debug("async_turn_off %s:%d", self.name, self._port)
         self._stop()
 
-    async def async_will_remove_from_hass(self):
-        _LOGGER.debug("async_will_remove_from_hass %s pid %d", self.name, self._pid)
+    async def async_stop(self):
+        _LOGGER.debug("async_stop %s:%d", self.name, self._port)
         self._stop()
 
-    async def on_remove(self):
-        _LOGGER.debug("_on_remove %s pid %d", self.name, self._pid)
+    async def async_will_remove_from_hass(self): # is this ever called?
+        _LOGGER.debug("async_will_remove_from_hass %s:%d", self.name, self._port)
+        # https://git.edevau.net/Ede_Vau/home-assistant/commit/5c3a4e3d10c5b0bfc0d5a10bfb64a4bfcc7aa62f?lang=en-US
+        await super().async_will_remove_from_hass()
+        await self.async_stop()
+
+    async def on_remove(self): # is this ever called?
+        _LOGGER.debug("_on_remove %s:%d", self.name, self._port)
         self._stop()
 
     def _stop(self):
-        if self._pid > 0:
-            os.kill(self._pid, signal.SIGKILL)
-            os.waitpid(self._pid, 0)
-            self._pid = 0
         self._state = STATE_OFF
+        if self._server:
+            self._server.close()
+            self._server = None
 
-    def _listen(self):
-        pid = os.fork()
-        if pid != 0:
-            return pid
-        # file:///usr/share/doc/python3-doc/html/library/socket.html
-        for res in socket.getaddrinfo(None, self._port, socket.AF_UNSPEC, socket.SOCK_STREAM, 0, socket.AI_PASSIVE):
-            af, socktype, proto, canonname, sa = res
-            try:
-                s = socket.socket(af, socktype, proto)
-            except OSError as msg:
-                s = None
-                continue
-            try:
-                s.bind(sa)
-                s.listen(1)
-            except OSError as msg:
-                s.close()
-                s = None
-                continue
-            break
-        if s is None:
-            _LOGGER.error("%s could not open socket", self.name)
-            sys.exit(7)
-        while True:
-            conn, addr = s.accept()
-            with conn:
-                _LOGGER.info("%s connection from %s", self.name, addr)
-        sys.exit(9)
+    async def handle_connection(self, reader, writer):
+        address = writer.get_extra_info('peername')
+        _LOGGER.debug('handle_connection {}:{} from {}:{}'.format(self.name, self._port, *address))
+        writer.close()
+
+    async def async_loop(self, server):
+        async with server:
+            await server.serve_forever()
+
+    async def async_listen(self):
+        _LOGGER.debug("async_listen %s:%d", self.name, self._port)
+        server = await asyncio.start_server(self.handle_connection, None, self._port)
+        _LOGGER.debug("async_listen %s:%d on %s", self.name, self._port, server.sockets[0].getsockname())
+        self._server = server
+        self._hass.async_create_task(self.async_loop(server))
+        _LOGGER.debug("async_listen done %s:%d", self.name, self._port)
