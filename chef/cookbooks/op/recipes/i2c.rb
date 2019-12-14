969a80cc228dc6db91a0a940ff091270ff0d1293
Stuart Pook <stuart12@users.noreply.github.com>
2019-12-14 16:30:28 +0100

    cleanup i2c.rb

--- chef/cookbooks/op/recipes/i2c.rb
+++ chef/cookbooks/op/recipes/i2c.rb
@@ -1,15 +1,15 @@
 ck = 'stuart'
 
-i2c = node[ck]['config']['i2c']['activate']
+activated = node[ck]['config']['i2c']['activate']
 
 %w[i2c-tools python3-paho-mqtt python3-rpi.gpio].each do |pkg|
   package pkg do
-    action i2c ? :upgrade : :remove
+    action activated ? :upgrade : :remove
   end
 end
 
 package 'python3-pip' do
-  action i2c ? :upgrade : :nothing
+  action activated ? :upgrade : :nothing
 end
 
 modules = ['i2c_dev']
@@ -20,62 +20,66 @@ template '/etc/modules-load.d/chef.conf' do
   )
   user 'root'
   mode 0o644
-  action i2c ? :create : :delete
+  action activated ? :create : :delete
 end
 
 modules.each do |module_name|
   # use kernel_module resource in Chef 14.3
   execute "modprobe #{module_name}" do
-    only_if { i2c }
+    only_if { activated }
     only_if { File.foreach('/proc/modules').grep(/^#{module_name}\W/).empty? }
   end
 end
 
 # https://docs.chef.io/resource_systemd_unit.html
-# rubocop:disable Metrics/BlockLength
+unit_service = {
+  UMask: '022',
+  RuntimeDirectory: '%N',
+  RuntimeDirectoryMode: '0700',
+  WorkingDirectory: '%t/%N',
+  Environment: 'HOME=%t/%N',
+  ExecStartPre: '/usr/bin/pip3 --no-cache-dir install adafruit-circuitpython-mcp9808',
+  DynamicUser: true,
+  Group: 'i2c',
+  DeviceAllow: 'char-i2c rw',
+  DevicePolicy: 'closed',
+  CapabilityBoundingSet: '',
+  NoNewPrivileges: true,
+  PrivateUsers: true,
+  ProtectControlGroups: true,
+  ProtectHome: true,
+  ProtectKernelModules: true,
+  ProtectKernelTunables: true,
+  RestrictAddressFamilies: 'AF_UNIX AF_INET AF_INET6',
+  RestrictRealtime: true,
+  MemoryDenyWriteExecute: true,
+  # SystemCallFilter: '@system-service',
+  TemporaryFileSystem: '/var:ro /docker:ro /media:ro',
+  InaccessiblePaths: '/mnt /boot',
+  MemoryHigh: '512M',
+  CPUQuota: '8%',
+  TasksMax: 6.to_s,
+}
 { 'home/bedroom/temperature' => 0x19, 'home/pi/temperature' => 0x1a }.each do |topic, address|
+  service = "mcp9809mqtt-#{address.to_s(16)}.service"
+  requires = ['mosquitto.service']
   scripts = ::File.join(node[ck]['config']['git']['directory'], 'github.com/stuart12', 'python-scripts')
-  systemd_unit "mcp9809mqtt-#{address.to_s(16)}.service" do
-    action i2c ? %w[create enable start] : %w[stop delete]
+  systemd_unit service do
+    action activated ? %w[create enable start] : %w[stop delete]
     content(
       Unit: {
         Description: 'Start mcp9809mqtt to publish temperatures from a MCP9808 to MQTT',
         Documentation: 'https://learn.adafruit.com/adafruit-mcp9808-precision-i2c-temperature-sensor-guide/python-circuitpython',
-        After: 'network-online.target',
+        After: requires,
+        Requires: requires,
       },
-      Service: {
-        UMask: '022',
-        RuntimeDirectory: '%N',
-        RuntimeDirectoryMode: '0700',
-        WorkingDirectory: '%t/%N',
-        Environment: 'HOME=%t/%N',
-        ExecStartPre: '/usr/bin/pip3 --no-cache-dir install adafruit-circuitpython-mcp9808',
+      Service: unit_service.merge(
         ExecStart: "#{scripts}/mcp9809mqtt --topic #{topic} --address 0x#{address.to_s(16)} --loglevel debug",
-        DynamicUser: 'true',
-        Group: 'i2c',
-        DeviceAllow: 'char-i2c rw',
-        DevicePolicy: 'closed',
-        CapabilityBoundingSet: '',
-        NoNewPrivileges: 'true',
-        PrivateUsers: 'true',
-        ProtectControlGroups: 'true',
-        ProtectHome: 'true',
-        ProtectKernelModules: 'true',
-        ProtectKernelTunables: 'true',
-        RestrictAddressFamilies: 'AF_UNIX AF_INET AF_INET6',
-        RestrictRealtime: 'true',
-        MemoryDenyWriteExecute: 'true',
-        # SystemCallFilter: '@system-service',
-        TemporaryFileSystem: '/var:ro /docker:ro /media:ro',
-        InaccessiblePaths: '/mnt /boot',
-        MemoryHigh: '512M',
-        CPUQuota: '8%',
-        TasksMax: 6.to_s,
-      },
+      ),
       Install: {
         WantedBy: 'multi-user.target',
       },
     )
+    notifies :restart, "systemd_unit[#{service}]" if activated
   end
 end
-# rubocop:enable Metrics/BlockLength
