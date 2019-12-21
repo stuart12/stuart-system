1ab5dc6c9c8faac8d7cd2dad63f99b5cd1b445a2
Stuart Pook <stuart12@users.noreply.github.com>
2019-12-21 14:51:19 +0100

    drop bootps & Spotify p2p, accept ipv6-icmp

--- chef/cookbooks/op/recipes/firewall.rb
+++ chef/cookbooks/op/recipes/firewall.rb
@@ -12,18 +12,46 @@ systemd_unit 'ferm.service' do
   action :nothing
 end
 
-template '/etc/ferm/ferm.conf' do
-  source 'ferm.conf.erb'
-  variables(
-    ipaddress: node['ipaddress'],
+addresses =
+  node['network']['interfaces']
+  .values
+  .select { |cfg| cfg['flags'].include?('BROADCAST') }
+  .map { |cfg| cfg['addresses'] }
+
+ipv4 = addresses
+  .map(&:values)
+  .flatten
+  .select { |v| v['family'] == 'inet' && v['scope'].casecmp('global').zero? }
+  .first
+# broadcast = ipv4['broadcast']
+prefixlen = ipv4['prefixlen']
+
+variables = {
+  ipaddress: node['ipaddress'],
+  prefixlen: prefixlen,
+  local: {
     tcp: {
       'ssh' => 26,
-      '1883' => 26, # mqtt/mosquitto,
+      1883 => 26, # mqtt/mosquitto,
     },
-    multicast: [
-      251, # mDNS
+    udp: {
+      21_027 => prefixlen, # Syncthing
+    },
+  },
+  drop: {
+    udp: [
+      'netbios-dgm',
+      57_621, # Spotify client P2P communication
     ],
-  )
+  },
+  multicast: {
+    251 => 'mdns',
+  },
+}
+
+template '/etc/ferm/ferm.conf' do
+  source 'ferm.conf.erb'
+  variables variables
   user 'root'
   mode 0o644
   notifies :restart, 'systemd_unit[ferm.service]' if activated
