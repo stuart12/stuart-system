b58f8adcc0bd4d8ce432f79e7fea02dab8fa6489
Stuart Pook <stuart12@users.noreply.github.com>
2019-12-22 21:42:55 +0100

    format 5kooka.rb

--- chef/cookbooks/op/attributes/5kooka.rb
+++ chef/cookbooks/op/attributes/5kooka.rb
@@ -3,7 +3,7 @@ ck = 'stuart'
 return unless node['filesystem']['by_mountpoint']['/']['uuid'] == '315c4bf7-9da3-4377-8c63-1d4005fce534'
 
 %w[
-vim-gtk3
+  vim-gtk3
 ].each do |pkg|
   default[ck]['config']['packages']['install'][pkg] = true
 end
