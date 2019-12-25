4d29ec08f45ffafe8f2430f7160350b8b19abfaf
Stuart Pook <stuart12@users.noreply.github.com>
2019-12-25 16:42:16 +0100

    force snapcast to use hw:DAC to avoid resampling

--- chef/cookbooks/op/attributes/5entrance.rb
+++ chef/cookbooks/op/attributes/5entrance.rb
@@ -3,6 +3,7 @@ ck = 'stuart'
 return unless node['filesystem']['by_mountpoint']['/']['uuid'] == '5337d0a6-2727-4039-beed-7b3513cd62c3'
 
 default[ck]['config']['snapclient']['activate'] = true
+default[ck]['config']['snapclient']['alsa_device'] = 'DAC'
 
 default[ck]['config']['networking']['ip'] = '192.168.0.30'
 default[ck]['config']['networking']['hostname'] = 'entrance'
--- chef/cookbooks/op/recipes/snapclient.rb
+++ chef/cookbooks/op/recipes/snapclient.rb
@@ -1,14 +1,36 @@
-ck = 'stuart'
+ck = node['stuart']
 name = 'snapclient'
-activated = node[ck].dig('config', name, 'activate')
+activated = ck.dig('config', name, 'activate')
 
 systemd_unit "#{name}.service" do
   action :nothing
 end
 
-file "/etc/default/#{name}" do
-  content "# Maintained by Chef\nSNAPCLIENT_OPTS='--hostID #{node.name}'\n"
+template "/etc/default/#{name}" do
+  variables(env: { 'SNAPCLIENT_OPTS' => "--hostID #{node.name}" })
+  source 'etc_default.erb'
   mode 0o444
-  notifies :restart, "systemd_unit[#{name}.service]"
-  only_if { activated }
+  user 'root'
+  notifies(:restart, "systemd_unit[#{name}.service]", :delayed) if activated
+  action activated ? :create : :delete
+end
+
+alsa_device = ck.dig('config', name, 'alsa_device')
+alsa_cfg_dir = '/etc/alsa/conf.d'
+
+[::File.dirname(alsa_cfg_dir), alsa_cfg_dir].each do |dname|
+  directory dname do
+    user 'root'
+    mode 0o755
+    action(activated && alsa_device ? :create : :nothing)
+  end
+end
+
+template ::File.join(alsa_cfg_dir, '99-chef.conf') do
+  source 'alsa.conf.erb'
+  action(activated && alsa_device ? :create : :delete)
+  variables(card: alsa_device)
+  user 'root'
+  mode 0o444
+  notifies(:restart, "systemd_unit[#{name}.service]", :delayed) if activated
 end
