723c9a828d798e308c06489e8ec5d81ffb4878fd
Stuart Pook <stuart12@users.noreply.github.com>
2019-12-26 09:09:05 +0100

    added comment on scanner data_portrange

--- chef/cookbooks/op/attributes/7scanner.rb
+++ chef/cookbooks/op/attributes/7scanner.rb
@@ -15,3 +15,20 @@ default[ck]['config']['udev']['rules'][name]['rules']['CanoScan9000F'] = [
 ].each do |pkg|
   default[ck]['config']['packages']['install'][pkg] = true
 end
+
+_debug_commands = <<~DEBUGCOMMANDS
+  /etc/sane.d/saned.conf:
+
+  Daemon options
+  Port range for the data connection. Choose a range inside [1024 - 65535].
+  Avoid specifying too large a range, for performance reasons.
+
+  ONLY use this if your saned server is sitting behind a firewall. If your
+  firewall is a Linux machine, we strongly recommend using the
+  Netfilter nf_conntrack_sane connection tracking module instead.
+
+  data_portrange = 10000 - 10100
+
+  ferm 2.4
+
+DEBUGCOMMANDS
