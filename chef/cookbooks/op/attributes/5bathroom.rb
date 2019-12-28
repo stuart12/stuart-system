7071872906ce787839bf2e7f67e2af509dabe79f
Stuart Pook <stuart12@users.noreply.github.com>
2019-12-28 22:05:50 +0100

    add snapcast to bathroom hass cfg

--- chef/cookbooks/op/attributes/5bathroom.rb
+++ chef/cookbooks/op/attributes/5bathroom.rb
@@ -1,12 +1,24 @@
 ck = 'stuart'
+name = 'bathroom'
 
 return unless node['filesystem']['by_mountpoint']['/']['uuid'] == '3442860e-35fd-41fd-b73d-30d4ccf50a8a'
 
-default[ck]['config']['networking']['hostname'] = 'bathroom'
+default[ck]['config']['networking']['hostname'] = name
 
 default[ck]['config']['boot']['config']['leds'] = false
 
 default[ck]['config']['snapclient']['activate'] = true
 default[ck]['config']['wifi']['activate'] = true
-default[ck]['config']['homeassistant']['activate'] = false
-default[ck]['config']['homeassistant']['keyboard'] = true
+
+CfgHelper.set_config['homeassistant'].tap do |hass|
+  hass['activate'] = true
+  hass['keyboard'] = true
+  hass['yaml'].tap do |yaml|
+    yaml['media_player'] = [
+      {
+        'platform' => 'snapcast',
+        'host' => 'kooka',
+      },
+    ]
+  end
+end
