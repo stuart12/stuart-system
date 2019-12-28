e4decb6d4152b3b27e2aa95eac19aab9a9b64cec
Stuart Pook <stuart12@users.noreply.github.com>
2019-12-28 20:33:05 +0100

    rework wifi to use attribute_driven for systemd

--- chef/cookbooks/op/attributes/7wifi.rb
+++ chef/cookbooks/op/attributes/7wifi.rb
@@ -1,18 +1,20 @@
 CfgHelper.set_config['wifi'].tap do |cfg|
   cfg['wpa_cfg'] = '/etc/wpa_supplicant/wpa_supplicant.conf'
   cfg['interface'] = 'wlan0'
-
-  cfg['systemd'].tap do |systemd|
-    systemd['Unit'].tap do |unit|
+end
+CfgHelper.set_config['systemd']['units']['wpa_supplicant.service'].tap do |srv|
+  srv['what'] = 'wifi'
+  srv['content'].tap do |content|
+    content['Unit'].tap do |unit|
       unit['Description'] = 'WPA supplicant maintained by Chef'
       unit['Before'] = 'network.target'
       unit['Wants'] = 'network.target'
     end
-    systemd['Service']['ExecStart'] = [
+    content['Service']['ExecStart'] = [
       '/sbin/wpa_supplicant -s',
       "-c#{CfgHelper.config['wifi']['wpa_cfg']}",
       "-i#{CfgHelper.config['wifi']['interface']}",
     ].join(' ')
-    systemd['Install']['WantedBy'] = 'multi-user.target'
+    content['Install']['WantedBy'] = 'multi-user.target'
   end
 end
--- chef/cookbooks/op/recipes/wifi.rb
+++ chef/cookbooks/op/recipes/wifi.rb
@@ -1,42 +1,29 @@
-# I could not get wpa_cli v2.8-devel to talk dBus to wpa_supplicant
-# so use a simplistic method that will only work with 1 wifi interface.
+return unless CfgHelper.activated? 'wifi'
 
-activated = CfgHelper.activated? 'wifi'
-
-%w[
+package %w[
   wpasupplicant
   iw
-].each do |pkg|
-  package pkg do
-    action activated ? :upgrade : :remove
-  end
-end
+]
 
+# I could not get wpa_cli v2.8-devel to talk dBus to wpa_supplicant
+# so use a simplistic method that will only work with 1 wifi interface.
+#
 # execute 'restart-wifi' do
 #   command "wpa_cli -i #{CfgHelper.config['wifi']['interface']} reconfigure"
 #   action :nothing
 # end
 
 template CfgHelper.config['wifi']['wpa_cfg'] do
-  if activated
-    variables(
-      networks: { node['secrets']['wifi']['ssid'] => node['secrets']['wifi']['psk'] },
-    )
-  end
+  variables(
+    networks: { node['secrets']['wifi']['ssid'] => node['secrets']['wifi']['psk'] },
+  )
   user 'root'
   group 'adm'
   mode 0o640
   # notifies(:run, 'execute[restart-wifi]') if activated
-  notifies(:restart, 'systemd_unit[wpa_supplicant.service]', :delayed) if activated
-  action activated ? :create : :delete
+  notifies(:restart, 'systemd_unit[wpa_supplicant.service]', :delayed)
 end
 
 execute 'rfkill unblock wlan' do
-  only_if { activated }
   only_if { ::File.read('/sys/class/net/wlan0/phy80211/rfkill0/soft').chomp == '1' }
-end
-
-systemd_unit 'wpa_supplicant.service' do
-  content CfgHelper.config['wifi']['systemd']
-  action activated ? :restart : %i[stop delete]
 end
