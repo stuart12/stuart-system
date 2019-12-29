c3072e0434e680cecec72a45e4df58ca6f74516b
Stuart Pook <stuart12@users.noreply.github.com>
2019-12-29 20:15:15 +0100

    put chef-solo tmp files in /tmp

--- chef/solo.rb
+++ chef/solo.rb
@@ -1,5 +1,5 @@
 root = File.absolute_path(File.dirname(__FILE__))
 
-file_cache_path ::File.join('/', 'var', 'tmp', ENV['USER'], 'chef-cache')
+file_cache_path ::File.join('/', 'tmp', ENV['USER'], 'chef-cache')
 cookbook_path root + '/cookbooks'
 role_path root + '/roles'
