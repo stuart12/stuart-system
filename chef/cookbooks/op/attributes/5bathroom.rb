334ecf5404fc49cff00f3460616dc8f8475dfe63
Stuart Pook <stuart12@users.noreply.github.com>
2020-01-03 08:54:19 +0100

    switch entrance amp on when changing volume on bathroom

--- chef/cookbooks/op/attributes/5bathroom.rb
+++ chef/cookbooks/op/attributes/5bathroom.rb
@@ -12,13 +12,11 @@ default[ck]['config']['wifi']['activate'] = true
 
 snap = "media_player.snapcast_client_#{name}"
 def mute_action(state, snap)
-  {
-    service: 'media_player.volume_mute',
+  { service: 'media_player.volume_mute',
     data: {
       entity_id: snap,
       is_volume_muted: state,
-    },
-  }
+    } }
 end
 
 %w[Enter Asterisk].map do |key|
@@ -33,13 +31,11 @@ end
     key,
     [
       mute_action(false, snap),
-      {
-        service: 'media_player.volume_set',
+      { service: 'media_player.volume_set',
         entity_id: snap,
         data_template: {
           volume_level: "{{ state_attr('#{snap}', 'volume_level') | float #{operation} 0.1 }}",
-        },
-      },
+        } },
     ],
   )
 end
@@ -51,11 +47,12 @@ end
     "Living Volume #{operation}",
     key,
     [
-      service: 'mqtt.publish',
-      data: {
-        topic: 'message',
-        payload: "living volume #{operation}",
-      },
+      mute_action(false, 'media_player.snapcast_client_entrance'),
+      { service: 'mqtt.publish',
+        data: {
+          topic: 'message',
+          payload: "living volume #{operation}",
+        } },
     ],
   )
 end
