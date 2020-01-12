bf8df9a0099d072e66654a787c321a6b12abc2b7
Stuart Pook <stuart12@users.noreply.github.com>
2020-01-12 22:33:15 +0100

    fix missing activated in scanner

--- chef/cookbooks/op/recipes/scanner.rb
+++ chef/cookbooks/op/recipes/scanner.rb
@@ -1,10 +1,10 @@
 return unless CfgHelper.activated? 'scanner'
 
 systemd_unit 'saned.socket' do
-  action activated ? %i[enable start] : %i[disable stop]
+  action %i[enable start]
 end
 
 file '/etc/sane.d/saned.conf' do
   content "# Maintained by Chef\nkooka\n"
-  action activated ? :create : :delete
+  action :create
 end
