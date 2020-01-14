692888414719dbf3a2ad24c24dd5c139c06a11c6
Stuart Pook <stuart12@users.noreply.github.com>
2020-01-14 20:33:12 +0100

    set kooka's hostname and network interface

--- chef/cookbooks/op/attributes/5kooka.rb
+++ chef/cookbooks/op/attributes/5kooka.rb
@@ -1,5 +1,10 @@
 return unless node['filesystem']['by_mountpoint']['/']['uuid'] == '315c4bf7-9da3-4377-8c63-1d4005fce534'
 
+CfgHelper.configure networking: {
+  hostname: 'kooka',
+  interface: 'eno1',
+}
+
 %w[
   vim-gtk3
 ].each do |pkg|
