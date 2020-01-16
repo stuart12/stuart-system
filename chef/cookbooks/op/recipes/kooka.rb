debb46a068c09eb5b90f18ee6df47fa624df62b6
Stuart Pook <stuart12@users.noreply.github.com>
2020-01-16 08:39:31 +0100

    systemd unit to run adb as user adb

--- chef/cookbooks/op/recipes/kooka.rb
+++ chef/cookbooks/op/recipes/kooka.rb
@@ -9,3 +9,60 @@ end
 systemd_unit 'ssh' do
   action %i[disable stop]
 end
+
+user = CfgHelper.configure({ name: 'adb', home: '/srv/adb', group: 'plugdev' }, %w[adb user])
+
+service = {
+  ExecStart: '/usr/bin/adb start-server',
+  Type: 'forking',
+  User: user['name'],
+  WorkingDirectory: user['home'],
+  SupplementaryGroups: user['group'],
+  ProtectSystem: 'full',
+  NoNewPrivileges: true,
+  PrivateTmp: true,
+  RestrictRealtime: true,
+  SystemCallArchitectures: 'native',
+  ProtectHome: true,
+  ProtectKernelTunables: false,
+  ProtectControlGroups: true,
+  ProtectKernelModules: true,
+  RestrictNamespaces: true,
+  CapabilityBoundingSet: '',
+  SystemCallFilter: '~@resources @privileged @obsolete @mount @clock @cpu-emulation @debug @keyring @module @raw-io',
+  MemoryDenyWriteExecute: true,
+  # DevicePolicy: 'closed',
+}
+
+hass = 'homeassistant'
+
+have_hass = 0.zero? # CfgHelper.activated?(hass)
+
+user user['name'] do
+  system true
+  home user['home']
+  manage_home true
+  comment 'adb runner'
+  action %i[create lock]
+  only_if { have_hass }
+end
+
+content = {
+  Unit: {
+    Description: "Run adb for #{hass}",
+    Before: "#{hass}.service",
+  },
+  Service: service,
+  Install: {
+    WantedBy: "#{hass}.service",
+  },
+}
+
+CfgHelper.configure({ adb: content }, %w[adb systemd]).transform_keys { |k| "#{k}.service" }.each do |name, cfg|
+  systemd_unit name do
+    content cfg
+    action %i[create enable]
+    notifies :restart, "systemd_unit[#{name}]", :delayed
+    only_if { have_hass }
+  end
+end
