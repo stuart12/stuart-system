39a8136d4e86d07730b056e1ae00056e37249282
Stuart Pook <stuart12@users.noreply.github.com>
2020-03-22 17:27:59 +0100

    remove outdated doc

--- chef/cookbooks/op/attributes/5spook7480latitude.rb
+++ chef/cookbooks/op/attributes/5spook7480latitude.rb
@@ -1,20 +1,5 @@
 return unless node['filesystem']['by_mountpoint']['/']['uuid'] == '9598eec9-7ec3-4b0f-b731-f5ee47716a3e'
 
-# Do a Debian install using the expert mode. Do not create users.
-# After configuring the disks, open a shell,
-# - umount /target/boot/efi
-# - umount /target/boot
-# - btrfs subvolume create /target/root
-# - mv /target/etc /target/media /target/root/
-# - btrfs subvolume list /target
-# - btrfs subvolume set-default XXX
-# - grep target /proc/mounts
-# - umount /target
-# - mount YYY /target
-# Continue the install, after the first boot
-# - checkout this repo
-# - run converge
-
 CfgHelper.activate 'desktop'
 CfgHelper.activate 'sshd'
 CfgHelper.activate 'vpn'
