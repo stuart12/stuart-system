030ce805ad304c20f323489890dfdf92a05d9254
Stuart Pook <stuart12@users.noreply.github.com>
2020-03-25 14:14:59 +0100

    put chef cache into /var/cache

--- chef/solo.rb
+++ chef/solo.rb
@@ -1,5 +1,5 @@
 root = File.absolute_path(File.dirname(__FILE__))
 
-file_cache_path ::File.join('/', 'tmp', ENV['USER'], 'chef-cache')
-cookbook_path root + '/cookbooks'
-role_path root + '/roles'
+file_cache_path ::File.join('/', 'var', 'cache', 'chef')
+cookbook_path ::File.join(root, 'cookbooks')
+role_path ::File.join(root, 'roles')
