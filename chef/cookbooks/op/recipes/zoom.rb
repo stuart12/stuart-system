060ae923beb9faa9613fc15393614a55acea3523
Stuart Pook <stuart12@users.noreply.github.com>
2020-03-27 13:41:08 +0100

    add dead code to install debug version of zoom

--- chef/cookbooks/op/recipes/zoom.rb
+++ chef/cookbooks/op/recipes/zoom.rb
@@ -3,6 +3,14 @@ return unless CfgHelper.activated? 'zoom'
 # https://zoom.us/
 # cloud platform for video and audio conferencing, collaboration, chat, and webinars
 
+if 1.zero?
+  CfgHelper.override(
+    %w[external zoom],
+    url: 'https://ota.zoom.us/Linux/64bit/zoom_withlog_latest_amd64.deb',
+    checksum: '7bfe38922aa9de76a725880cddc2c48fa6d109279d36c4c20a6f8b051f22ebe7',
+  )
+end
+
 cfg = CfgHelper.attributes(
   %w[external zoom],
   url: 'https://zoom.us/client/latest/zoom_amd64.deb',
