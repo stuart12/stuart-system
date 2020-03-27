6a849a4da8ff62326b279e03f9fbd1148ae8cb59
Stuart Pook <stuart12@users.noreply.github.com>
2020-03-27 20:47:38 +0100

    update for 0.80.1

--- chef/cookbooks/op/recipes/vpn.rb
+++ chef/cookbooks/op/recipes/vpn.rb
@@ -4,13 +4,13 @@ package 'vpnc' do
   action :upgrade
 end
 
-def hash_to_2dim_array(h, c = [])
-  if h.is_a?(Hash)
-    h.flat_map do |k, v|
-      hash_to_2dim_array(v, c + [k])
+def hash_to_2dim_array(remaining, path = [])
+  if remaining.is_a?(Hash)
+    remaining.flat_map do |k, v|
+      hash_to_2dim_array(v, path + [k])
     end
   else
-    [c + [h]]
+    [path + [remaining]]
   end
 end
 # hash_to_2dim_array(1 => 4, 5 => { 0 => 6, 2 => 3 }, 12 => { 5 => 3, 13 => { 14 => 15 } })
