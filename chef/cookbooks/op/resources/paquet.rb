3737cded5b93d63bf7153106e3214bbc70290561
Stuart Pook <stuart12@users.noreply.github.com>
2020-04-05 00:14:42 +0200

    fix reuse of package in paquet

--- chef/cookbooks/op/resources/paquet.rb
+++ chef/cookbooks/op/resources/paquet.rb
@@ -2,13 +2,13 @@
 
 resource_name :paquet
 
-property :package, String, name_property: true
+property :pkg_name, String, name_property: true
 # https://discourse.chef.io/t/can-you-access-node-attributes-from-within-a-chef-custom-resource-definition/10247/2
 # FIXME: don't use stuart, how to call a helper from a Custom Resource?
 property :suppressed, Hash, default: lazy { node['stuart'].dig('config', 'package', 'suppress') || {} }
 
 action :install do
-  package new_resource.package do
-    not_if { new_resource.suppressed[new_resource.package] }
+  package new_resource.pkg_name do
+    not_if { new_resource.suppressed[new_resource.pkg_name] }
   end
 end
