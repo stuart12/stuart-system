db377fa152cb1ccfa13b98868d7f6269392cc8b9
Stuart Pook <stuart12@users.noreply.github.com>
2020-04-04 17:12:19 +0200

    cleanup git_root

--- chef/cookbooks/op/attributes/default.rb
+++ chef/cookbooks/op/attributes/default.rb
@@ -3,7 +3,6 @@ CfgHelper.attributes(
   bin: '/usr/local/bin',
 )
 
-git_root = '/opt/chef-git'
 CfgHelper.attributes(
   [],
   workstation: 'kooka',
@@ -13,14 +12,6 @@ CfgHelper.attributes(
   timezone: {
     name: 'Europe/Paris',
   },
-  'git-stuart' => {
-    root: File.join(git_root, 'github.com', 'stuart12'),
-  },
-)
-
-CfgHelper.attributes(
-  %w[git],
-  directory: git_root,
 )
 CfgHelper.my_repo('python-scripts')
 
@@ -55,12 +46,12 @@ CfgHelper.attributes(
 ].each do |pkg|
   CfgHelper.add_package pkg
 end
-# rubocop:disable LineLength
+# rubocop:disable Layout/LineLength
 CfgHelper.attributes(
   %w[users ssh_keys],
   'stuart@kooka' => 'ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA34JVpS1wfN99SxuuwKQBKgUxOu6JmJq1ANO/A+/Iypabn3AbEyeXUCJPQe+VY/p8Opk01Ywm6h/vAQgHILGVDsBR0EQ4Ku444PXACBfdLkShclOR3LIUq3BDaZq1LJTryPQhqhnTBhLUvUvQy4RtLB8grKIEMcyGRcAr+1Uo4Bf+VU2VdFbW+dJb45jM66pd20/tmhBTFwHee8BO32nJKYHXdmgHOIci29bzPBJGnD0M3HFzbh9qgCLuTCWx9/77TogO28TPhoVP7BoOAji4YJxxzT/0CMeAoTRQaFB2aMAo56Ix+Pxnx2GYDq83NUWjjVBnTovlxIXxK7VcSlt7WQ==',
 )
-# rubocop:enable LineLength
+# rubocop:enable Layout/LineLength
 
 CfgHelper.attributes(
   %w[work],
