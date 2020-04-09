11dc59cdb1e2d82e873d0ca341a67ff82e959287
Stuart Pook <stuart12@users.noreply.github.com>
2020-04-09 13:35:58 +0200

    make paquet resource use helpers

--- chef/cookbooks/op/resources/paquet.rb
+++ chef/cookbooks/op/resources/paquet.rb
@@ -1,14 +1,20 @@
-# Install a package unless it has been suppressed via an attribute
+# Install a package or its possible replacement
 
 resource_name :paquet
 
+action_class do
+  # https://docs.chef.io/custom_resources/
+  include StuartConfig::Helpers::CfgHelper
+end
+
 property :pkg_name, String, name_property: true
-# https://discourse.chef.io/t/can-you-access-node-attributes-from-within-a-chef-custom-resource-definition/10247/2
-# FIXME: don't use stuart, how to call a helper from a Custom Resource?
-property :suppressed, Hash, default: lazy { node['stuart'].dig('config', 'package', 'suppress') || {} }
 
 action :install do
-  package new_resource.pkg_name do
-    not_if { new_resource.suppressed[new_resource.pkg_name] }
+  replacement = config(%w[package replace])[new_resource.pkg_name]
+
+  if replacement.nil? || replacement
+    package replacement || new_resource.pkg_name do
+      action :upgrade
+    end
   end
 end
