2c1db028eb9d8271a6434060bc9f1d7f1221d1d8
Stuart Pook <stuart12@users.noreply.github.com>
2020-04-09 19:43:19 +0200

    add resource remote_deb to retrieve & install debs

---
+++ chef/cookbooks/op/resources/remote_deb.rb
@@ -0,0 +1,48 @@
+resource_name :remote_deb
+
+action_class do
+  include StuartConfig::Helpers::CfgHelper # https://docs.chef.io/custom_resources/
+end
+
+property :name, String, name_property: true
+property :url, String
+property :checksum, String
+property :options, [String, Array], default: []
+property :dependencies, Array, default: []
+property :hide, [String, Array], default: []
+property :delete, [String, Array], default: []
+
+action :manage do
+  new_resource.dependencies.each do |pkg|
+    paquet pkg
+  end
+
+  deb = ::File.join(Chef::Config[:file_cache_path], "#{new_resource.name}.deb")
+
+  remote_file deb do
+    source new_resource.url
+    checksum new_resource.checksum
+    owner 'root'
+    mode 0o644
+    notifies :remove, "dpkg_package[#{new_resource.name}]", :immediately
+  end
+
+  dpkg_package new_resource.name do
+    source deb
+    options new_resource.options + ['--no-triggers']
+  end
+
+  group = config(%w[work group])
+  [new_resource.hide].flatten.each do |fname|
+    directory fname do
+      group group
+      mode 0o754
+    end
+  end
+
+  [new_resource.delete].flatten.each do |fname|
+    file fname do
+      action :delete
+    end
+  end
+end
