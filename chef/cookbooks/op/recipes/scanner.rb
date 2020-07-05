93c7f5b6986e4a61ab2278b243eaceb8896d13dc
Stuart Pook <stuart12@users.noreply.github.com>
2020-07-05 15:03:24 +0200

    default scand client is localhost

--- chef/cookbooks/op/recipes/scanner.rb
+++ chef/cookbooks/op/recipes/scanner.rb
@@ -1,11 +1,16 @@
 return unless CfgHelper.activated? 'scanner'
 
+clients = CfgHelper.attributes(
+  %w[scanner clients],
+  localhost: 'localhost',
+).compact.values.sort.uniq.join('\n')
+
 systemd_unit 'saned.socket' do
   action %i[enable start]
 end
 
 file '/etc/sane.d/saned.conf' do
-  content "# Maintained by Chef\ndata_portrange=20109-20110\n#{CfgHelper.workstation}\n"
+  content "# Maintained by Chef\ndata_portrange=20109-20110\n#{clients}\n"
   action :create
   notifies :restart, 'systemd_unit[saned.socket]', :delayed
 end
