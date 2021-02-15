1a8009de089cde2a053b8e9960170faf1cb2714c
Stuart Pook <stuart12@users.noreply.github.com>
2021-02-15 16:01:28 -0300

    cookbook to force a version of npm

---
+++ chef/cookbooks/op/recipes/npm.rb
@@ -0,0 +1,29 @@
+return unless CfgHelper.activated? 'npm'
+
+version = '6.6.0'
+home = ::File.join(Chef::Config[:file_cache_path], 'npm-hack')
+where = '/lib/npm-chef'
+executable = ::File.join(where, 'bin/npm')
+
+execute [
+  "rm -rf #{home}",
+  "mkdir -p -m 0700 #{home}/.cache/npm/lib",
+  "npm install --cache /tmp --global npm@#{version}",
+  "rm -rf #{where}",
+  "cp -r #{home}/.cache/npm #{where}",
+  "chmod -R og=u,og-w #{where}",
+  "rm -r #{home}",
+].join(' && ') do
+  not_if { ::File.exist?(executable) && version == `#{executable} --version`.strip }
+  environment(
+    {
+      'HOME' => home,
+    },
+  )
+end
+
+file '/etc/profile.d/chef-npm' do
+  content "PATH=#{where}/bin:$PATH\n"
+  mode 0o644
+  owner 'root'
+end
