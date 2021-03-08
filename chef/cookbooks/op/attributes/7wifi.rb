5a8d7fce450f0e2718c3a86a49872bab715159c4
Stuart Pook <stuart12@users.noreply.github.com>
2021-03-08 15:43:48 -0300

    fix wifi interface name

--- chef/cookbooks/op/attributes/7wifi.rb
+++ chef/cookbooks/op/attributes/7wifi.rb
@@ -2,7 +2,7 @@ return unless CfgHelper.activated? 'wifi'
 
 CfgHelper.set_config['wifi'].tap do |cfg|
   cfg['wpa_cfg'] = '/etc/wpa_supplicant/wpa_supplicant.conf'
-  cfg['interface'] = 'wlan0'
+  cfg['interface'] = 'wlp2s0'
 end
 CfgHelper.set_config['systemd']['units']['wpa_supplicant.service'].tap do |srv|
   srv['what'] = 'wifi'
