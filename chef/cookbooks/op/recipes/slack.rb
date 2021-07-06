101d0b08cd56a13e3d85daf42c3ecdf6b9050578
Stuart Pook <stuart12@users.noreply.github.com>
2021-07-06 09:36:43 +0200

    upgrade slack

--- chef/cookbooks/op/recipes/slack.rb
+++ chef/cookbooks/op/recipes/slack.rb
@@ -2,10 +2,10 @@ return unless CfgHelper.activated? 'slack'
 
 cfg = CfgHelper.attributes(
   ['slack'],
-  url: 'https://downloads.slack-edge.com/linux_releases/slack-desktop-4.4.0-amd64.deb',
+  url: 'https://downloads.slack-edge.com/linux_releases/slack-desktop-4.17.0-amd64.deb',
   hide: '/usr/lib/slack',
   delete: '/etc/cron.daily/slack',
-  checksum: '22dc90c14a845b9d789952d2c97d76bd54a350062a6d8d9b51097b91fa735940',
+  checksum: 'b1e7123f9e51d292b647fecd42236f2de3b3f863c631e8278d47e08b8aae8c1d',
 )
 remote_deb 'slack' do
   url cfg['url']
